65152fc277e44d4e6778efc419189c37113ef5d0
Revyiii <96985670+Revyiii@users.noreply.github.com>
2023-02-25 18:37:28 +0100

    Update 1.6.cpp

--- Zadania/Część pierwsza/1.6.cpp
+++ Zadania/Część pierwsza/1.6.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <iomanip>
+#include <locale.h>
 
 #using namespace std
 
-int main(){
-cout<<"test"<<endl;
+int main()
+{
+setlocale(LC_CTYPE,"Polish");
+float a;
+float b;
+float c;
+
+cout<<"Prosze podać bok a:";
+cin >> a;
+cout <<"Prosze podać bok b:"; 
+cin >> b;
+cout <<"Prosze podać bok c:";
+cin >> c;
+cout>>fixed<<setprecision(2)<<\
+"Objętość jest równa="<<a*b*c<<endl<<\
+"pole jest równe="<<2*(a*b+a*c+c*d)<<endl<<\
+"Suma długości krawędzi to="<<(a+b+c)*4;
+
 cin.get();
 }
