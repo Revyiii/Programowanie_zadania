158ea2c899fef59af5ec8c82d09c0fc740a5b957
Revyiii <96985670+Revyiii@users.noreply.github.com>
2023-02-28 00:45:12 +0100

    allmost

--- Zadania/Część pierwsza/1.14.cpp
+++ Zadania/Część pierwsza/1.14.cpp
@@ -1,34 +1,58 @@
 #include <iostream>
 #include <locale.h>
 #include <cmath>
-
+#include <windows.h>
 using namespace std;
-void print_arr(int temp_arr[],int l)
+void gotoxy(int x, int y)
+{
+COORD cord;
+cord.X = x;
+cord.Y = y;
+SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cord);
+}
+
+void print_arr(float temp_arr[],int l)
 {
     int n=0;
     while (n<l)
 	{
 	    //<sizeof(temp_arr)/sizeof(temp_arr[0])
-		cout << temp_arr[n] << ",";
+		cout << temp_arr[n-1] << ",";
 		n++;
 	}
 	cout<<endl;
 }
 
-
 int main() 
 {
 	setlocale(LC_CTYPE,"Polish");
-	int min = 0;
+	int min = 1;
 	int max = 10;
 	int n = 3;
-	float arr = [n];
-	for(i=0;i<3;i++)
-	{
-		arr[i-1]={celi(a+rand()%(min-max+1)*10)/10};
-	}
-	print_arr(arr,sizeof(arr)/sizeof(arr[0]);
+	float a[n];
+	float b;
+	int c;
 	
-	cin.get();
+	while(c==1)
+	{
+	
+	for(int i=0;i<n;i++)
+	{	
+		a[i-1]={ceil((rand()/ double(RAND_MAX))*100)/10+1};
+	}
+	
+	for(int i=0;i<n;i++)
+	{for(int j=0;j<n;j++)
+	{if(a[i-1]<a[j-1])
+	{b=a[i-1];a[i-1]=a[j-1];a[j-1]=b;}}}
+	
+	gotoxy(0,0);
+	print_arr(a,n);
+	
+	gotoxy(0,28);
+	if(a[-1]+a[0]>a[1]){cout<<"tak"<<endl;}
+	else{cout<<"nie"<<endl;}
+	cin>>c;
+	}
 	return 0;
 }
