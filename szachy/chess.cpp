4802168506cadc930ef40788d568e26c720cefbb
Revyiii <96985670+Revyiii@users.noreply.github.com>
2023-03-09 00:28:35 +0100

    late night typing

--- szachy/chess.cpp
+++ szachy/chess.cpp
@@ -4,6 +4,18 @@
 
 using namespace std;
 
+
+int plane[64] = {\
+4,2,3,5,6,3,2,4,\
+1,1,1,1,1,1,1,1,\
+0,0,0,0,0,0,0,0,\
+0,0,0,0,0,0,0,0,\
+0,0,0,0,0,0,0,0,\
+0,0,0,0,0,0,0,0,\
+7,7,7,7,7,7,7,7,\
+10,8,9,11,12,9,8,10,\
+};
+int color;
 void gotoxy(int x, int y)
 {
 COORD cord;
@@ -12,49 +24,46 @@ cord.Y = y;
 SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cord);
 }
 
-void animat_plane_w(int pla[])
+void animat_plane()
 {
 	gotoxy(0,0);
 	int c;
-	int r=55;
-	while(r>=-1)
+	int n;
+	int r;
+	char letter;
+	if (color==-1){r=8;letter=104;/*h czarne*/}
+	else{r=55;letter=97;/*a białe */}
+	cout<<"color: "<< color << endl;//debug
+	while(r>=-1 && r<=64)
 	{
 		c=r;
-		cout<<(r+1)/8+1<<"  ";
-		while(c<r+8)
+		n=0;
+		cout<<(r+1)/9-((color+1)/2-1)<<"  ";//round(pow(1/2,color))-1
+		while(n<8)
 		{
-		c++;
-		cout<<pla[c] <<",";		
+		c=c+1*color;
+		cout<<plane[c] <<",";		
+		n++;
 		}
-		r=r-8;
+		r=r-8*color;
 		cout<<endl;
 	}
-	cout<<endl<<"   a,b,c,d,e,f,g,h"<<endl;
-}
-void animat_plane_b(int pla[])
-{
-	gotoxy(0,0);
-	int c;
-	int r=8;
-	while(r<=64)
+	cout<<endl<<".  ";	
+	for(int n=0;n<8;n++)
 	{
-		c=r-1;
-		cout<<(r+1)/8<<"  ";
-		while(c>=r-8)
-		{
-		
-		cout<<pla[c]<<",";	
-		c--;	
-		}
-		r=r+8;
-		cout<<endl;
+	cout<<letter<<",";
+	letter = letter+color;
 	}
-	cout<<endl<<"   h,g,f,e,d,c,b,a"<<endl;
+	cout<<endl;
 }
-
-float white_move(string pow,int plane[])
+void move()
 {
-    char* pow_arr = new char[pow.length()];
+    char letter=98-((color+1)/2-1);//a
+	string pow;
+	cout << letter<<":";
+	cin>>pow;
+	
+	char* pow_arr = new char[pow.length()];
     strcpy(pow_arr, pow.c_str());
     
     int figure=0;
@@ -62,82 +71,25 @@ float white_move(string pow,int plane[])
     int row=pow_arr[pow.length()-1]-48;
     int pole=8*(row-1)+colum-1;
 	
-	if(plane[pole-8]==1)
-	{
-		
-		plane[pole]=1;
-		plane[pole-8]=0;
-		animat_plane_w(plane);
-		gotoxy(0,13);
-	}
-	else if (plane[pole-16]==1) 
-	{
-		//if
-		plane[pole]={1};
-		plane[pole-16]={0};
-		animat_plane_w(plane);
-		gotoxy(0,13);	
-	}
-	else{cout<<"bad move"<< endl;return-1;}
 	delete[] pow_arr;
 }
 
 
 int main()
 {
-	int n=0;
-	int plane[64] = {\
-	4,2,3,5,6,3,2,4,\
-	1,1,1,1,1,1,1,1,\
-	0,0,0,0,0,0,0,0,\
-	0,0,0,0,0,0,0,0,\
-	0,0,0,0,0,0,0,0,\
-	0,0,0,0,0,0,0,0,\
-	7,7,7,7,7,7,7,7,\
-	10,8,9,11,12,9,8,10,\
-	};
-	float pole = 0;
-	bool mate=false;
-	string pow;
-	int p1=0;
-	int p2=0;
-	
+	bool mate=false;	
 	while (mate==false)
 	{
-	n++;
+	color=1;//białe
 	system("cls");
-	gotoxy(0,0);
-	animat_plane_w(plane);
-	pole=-1;
-	while(pole<0)
-	{
-	cout <<"w:";
-	cin>>pow;
-	pole=white_move(pow,plane);
-	}
-	p1=floor(pole);
-	p2=pole-floor(pole);
-	plane[p1]=plane[p2];
-	animat_plane_w(plane);	
-	
-	//-------------ruch-------------
-	
-	
-	//cout <<"figure: "<< figure<<endl<<"row: "<<row<<endl<<"colum: "<<colum<<endl<<"pole: "<<pole<<endl;
-    cout <<"b:";
-	cin>>pow;   
-	
+	animat_plane();
+	move();
+
+	color=-1;//czarne
 	system("cls");
-	animat_plane_b(plane);	
-	//================CZARNY RUCH=====================
-	
-	
-	
-	
+	animat_plane();
+	move();
 	}
-
-	
-
 	cin.get();
 	return 0;
 }
