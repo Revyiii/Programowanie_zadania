8f6d349ae0ee0df22170c785a680d4eec12a26a6
Revyiii <96985670+Revyiii@users.noreply.github.com>
2023-10-05 07:05:06 +0200

    asd

--- Zadania/Część druga/2.11.2.cpp
+++ Zadania/Część druga/2.11.2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <vector>
+#include <math.h>
 using namespace std;
 
 int main()
@@ -9,17 +10,18 @@ int main()
 	int i;
     int a;
     int min;
+    vector<int> s;
     
     cout<<"program do szukania minimalne liczby z ciągu liczb dwu cyfrowych"<<endl;  
     cout<<"aby zakończyć podawanie ciągu podaj liczbę 0"<<endl;  
     while(true)
     {
-        i=0;
-        min=100;
-        
+    i=0;
+    a=100;
+    min=100;
+	s.clear();    
     while(a!=0)
     {
-        
         i++;
         cin.clear();cin.sync();
         cout<<"podaj "<<i<<":";
@@ -27,11 +29,16 @@ int main()
         
         if(a==0){break;}
         else if(cin.fail()||abs(a)<10||abs(a)>99){cout<<"podana wartość jest nie poprawna"<<endl;i--;}
-        else{if(a<min){min=a;}}
+        else{s.push_back(a);if(a<min){min=a;}}
     }
-    a=100;
+	a=100;
     cout<<"minimalną liczbą z ciągu:"<<endl;
-    if(min==100){cout<<"najmniejsza liczba ci¹gu nale¿y do zbioru pustego"<<endl;}
+    for (int ii = 0; ii < s.size(); ii++)
+    {
+        cout<<s.at(ii)<<", ";
+        if(s.at(ii)<a){a=s.at(ii);}
+    }
+    if(a==100){cout<<"najmniejsza liczba ci�gu nale�y do zbioru pustego"<<endl;}
 	else{cout<<endl<<"jest: "<<min<<endl;}
     
     cout<<"jeszcze raz? T/N: ";do{cin.clear();cin.sync();cin>>d;d=toupper(d);if(d=='N'){return 0;}else if(d=='T'){break;}else{cout<<"podana warto�� jest nie poprawna. Pon�w prub�"<<endl;}}while(true);}
