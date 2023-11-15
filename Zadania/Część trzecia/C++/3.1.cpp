f0a5986298d210e37d72fa879dcfffc782df2014
Revyiii <96985670+Revyiii@users.noreply.github.com>
2023-11-15 17:55:27 +0100

    gwiazdki fianal
    gwiazdki juz no more

--- Zadania/Część trzecia/C++/3.1.cpp
+++ Zadania/Część trzecia/C++/3.1.cpp
@@ -1,38 +1,63 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+void RL(float a,float b)
+{	
+ if(a==0){
+ if(b==0){cout<<"R�wnanie to�samo��owe"<<endl;}
+ else{cout<<"r�wnanie sprzeczne"<< endl;}}
+ else{
+ if(b==0){cout<<"x jest r�wny:0"<<endl; }
+ else{a=b/a;cout<<"x jest r�wny: "<<a<<endl;}}
+}
+void RK(float a, float b, float c)
+{
+float delta=b*b-(4.0*a*c);	
+
+if(delta==0)
+{
+a=((-b)/(2*a));
+cout<<"x jest r�wny: "<< a <<endl;
+}else{
+if(delta>0)
+{cout<<"x1 jest r�wny: "<< ((-b-sqrt(delta))/(2*a))<<endl;
+cout<<"x2 jest r�wny: "<<((-b+sqrt(delta))/(2*a))<<endl;
+}else
+{cout<<"brak rozwi�z�"<<endl;}}
+}
+
 int main()
 {
-    cout<<endl<<"program do liczenie n liczby fibonacciego"<<endl;
-    bool r = true;
+    cout<<endl<<"program do liczenie równania"<<endl;
     char d;    
-    while(r==true)
+    int i; 
+    char l;
+    int x[10];
+    while(true)
     {
-    cout<<"\npodaj n:";
-    int n = 0;
-    cin>>n;
-    if(cin.fail()){r=false;cout<<endl<<"podana wartość jest nie poprawna"<<endl;}
-    int i = 0;
-    double a=0;
-    double b=1;
-    double c=0;
-    while (n>i)
+    l='a';
+    i=0;
+    while(i<sizeof(x)/sizeof(x[1]))
     {
-        c=a+b;
-        a=b;
-        b=c;
-        i++;
+    cout<<"podaj "<<l<<":";
+    cin.clear();cin.sync();cin>>x[i];
+    if(cin.fail())
+    {i--;l--;cout<<"podana wartość jest nie poprawna"<<endl;}
+    i++;l++;
     }
-    cout<<n<<" liczba ciagu fibonacciego jest rowna:"<<a<<endl;
+    cout<<"a:"<<x[0]<<" b:"<<x[1]<<" c:"<<x[2]<<" d:"<<x[3]<<endl;
+	x[2]=x[2]-x[3];
+	if(x[0]==0){RL(x[1],x[2]);}
+	else{RK(x[0],x[1],x[2]);}
+
+
     cout<<"jeszcze raz? T/N: ";
-    do{
-		cin.clear();cin.sync();
-		cin>>d;
-		d=toupper(d);
+    do{cin.clear();cin.sync();cin>>d;d=toupper(d);
 		if(d=='N'){return 0;}
 		else if(d=='T'){break;}
-		else{cout<<"podana warto�� jest nie poprawna. Pon�w prub�"<<endl;}
+		else{cout<<"podana wartartość jest nie poprawna. Ponów prubu"<<endl;}
 		}while(true);
     }
 	cin.get();
---
+++ Zadania/Część trzecia/C++/3.2 gra w gwiazdki.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <math.h>
+#include <windows.h>
+
+using namespace std;
+
+void gotoxy(int x, int y)
+{
+    COORD cord;
+    cord.X = x;
+    cord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cord);
+}   
+
+int getrc(bool pick) // 0=columns 1=rows
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int columns, rows;
+
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    if(pick==0){return columns;}
+    else{return rows;}
+}
+
+int getx()
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    int x=csbi.dwCursorPosition.X;  
+	return x;
+}
+int gety()
+{
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    int y=csbi.dwCursorPosition.Y;  
+	return y;
+}
+
+void clearLine(int y)
+{
+    int i=0;
+    int c=getrc(0);
+    gotoxy(0,y);
+    while(i<c)
+    {
+        cout<<" ";
+        i++;
+    }
+}
+
+int get(int n){
+int a;
+while(true){
+cout<<"gracz nr."<<n<<" podaj liczbe calkowita od 1 do 50:";
+cin.clear();cin.sync();cin>>a;
+if(cin.fail()||a>50||a<1){clearLine(gety()-1);gotoxy(0,gety());cout<<"podana wartosc jest zla spruboj ponownie ";}
+else{gotoxy(0,gety()-1);cout<<"gracz nr."<<n<<" podaj liczbe calkowita od 1 do 50:##"<<endl;return a;}
+}
+}
+
+void komentarz(int x,int y, int n){
+    gotoxy(x,y);
+    switch (n)
+    {
+    case 0:cout<<"wygral!!!"<<endl;break;
+    case 1:cout<<"przegral podal zamalo"<<endl;break;
+    case 2:cout<<"przegral podal za duzo"<<endl;break;
+    case 3:cout<<"remis"<<endl;break;
+    default:cout<<"cos poszło nie tak"<<endl;break;
+    }
+}
+
+void gwiazdki(int x,int y, int n){
+    gotoxy(x,y);
+    int i=1;
+    while(i<=n){
+        cout<<"*";
+        i++;
+    }
+}
+
+void sprawdz(int k,int g1,int g2, int &o1, int &o2)
+{
+bool a=floor(g1/k);
+bool b=floor(g2/k);
+if (a+b==2){o1=3;o2=3;}
+else if(a==1){o1=2;o2=0;}
+else if(b==1){o1=0;o2=2;}
+else if(g1>g2){o1=0;o2=1;}
+else if(g1<g2){o1=1;o2=0;}
+else if(g1==g2){o1=3;o2=3;}
+else{cout<<"coś poszło nie tak"<<endl;}
+}
+
+int main(){
+    cout<<endl<<"Witam w grze w gwiazdki"<<endl;//fancy intro maybe?
+    char d;    
+    int g1;
+    int g2;
+    int o1;
+    int o2;
+    int w1=0;
+    int w2=0;
+    int k;
+    while(true)
+    {
+        g1=get(1);
+        g2=get(2);
+        k=1+rand()%(50);
+        cout<<"k:";gwiazdki(4,gety(),k);
+        cout<<endl<<"P1:";gwiazdki(4,gety(),g1);
+        cout<<endl<<"P2:";gwiazdki(4,gety(),g2);
+        cout<<endl;
+        
+        sprawdz(k,g1,g2,o1,o2);
+        gotoxy(0,gety()+1);
+        cout<<"gracz 1 ";komentarz(8,gety(),o1);
+        cout<<"gracz 2 ";komentarz(8,gety(),o2);
+        if(o1==0){w1++;}
+        else if(o2==0){w2++;}
+
+    cout<<"jeszcze raz? T/N: ";
+    do{cin.clear();cin.sync();cin>>d;d=toupper(d);
+		if(d=='N'){return 0;}
+		else if(d=='T'){system("cls");gotoxy(0,0);cout<<"g1:"<<w1<<" g2:"<<w2<<endl;break;}
+		else{cout<<"podana wartartość jest nie poprawna. Ponów prubu"<<endl;}
+		}while(true);
+    }
+	cin.get();
+    return 0;
+}   
