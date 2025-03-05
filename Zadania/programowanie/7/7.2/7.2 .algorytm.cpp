fc1c1c0dc4d0f4bafa676c7b31415a38d8240c8e
Revyiii <96985670+Revyiii@users.noreply.github.com>
2025-03-05 10:27:45 +0100

    dfg

--- Zadania/programowanie/7/7.2/7.2 .algorytm.cpp
+++ Zadania/programowanie/7/7.2/7.2 .algorytm.cpp
@@ -5,18 +5,22 @@ using namespace std;
 int main(){
 	cout<<endl<<"Witam w zadaniu 7.2"<<endl;
 	
-	int i =0;
+	int i =1;
 	string w="";
-
-	string s="matur";
-	while(i<=s.size())
+	int k=2;
+	int j;
+	string s=" arka";
+	while(i<=k){
+	j=i;
+	while(j<=s.size())
 	{
-		w=w+s[i+1];
-		w=w+s[i];
-		cout<<w<<endl;
-		i=i+2;
+		w=w+s[j];
+		j=j+k;
+		cout<<w<<endl;	
 	}
-
+	i++;
+	
+	}
 	cin.get();
 	return 0;
 }   
