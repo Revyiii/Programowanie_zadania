f1af7e8ccddfdf3395b9a22d1842ce9f4188388c
Revyiii <96985670+Revyiii@users.noreply.github.com>
2025-04-16 09:38:20 +0200

    fsadf

---
+++ Zadania/programowanie/9/8.1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+string nasystem(int l, int p)
+{
+	string s="";
+	int n = l.size();
+	
+	return s;
+}
+
+int main()
+{
+	int l = 123;
+	int p = 2;
+	nasystem(l,p);
+}
