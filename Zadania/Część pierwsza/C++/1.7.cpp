17147d8810e447fb09a5ee9bb38c890aeaf524d0
Tymon Łazowy <96985670+Revyiii@users.noreply.github.com>
2025-11-16 09:50:30 +0100

    bhj

--- Zadania/Część pierwsza/C++/1.7.cpp
+++ Zadania/Część pierwsza/C++/1.7.cpp
@@ -1,23 +1,15 @@
 #include <iostream>
-#include <conio.h>
-#include <locale.h>
-#include <iomanip>
+
 using namespace std;
 
 int main()
 {
-setlocale(LC_CTYPE,"Polish");
 float r;
 float h;
 const float pi=3.14;
 
 cout<<"Prosze podać promień:"; cin >> r;
 cout <<"Prosze podac wysokosc:"; cin >> h;
-cout<<fixed<<setprecision(2)<<\
-"Objetosc jest równa="<<pi*r*r*h<<endl<<\
-"pole jest równe="<<2*pi*r*(r+h)<<endl<<\
-"Suma dlugosci krawedzi to="<<4*pi*r;
-
 cin.get();
 return 0;
 }
